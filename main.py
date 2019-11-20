77eef3642f9cf6bbd454d5875f333b376d55a865
ntanetani <n.tanetani@gmail.com>
2019-11-20 12:35:10 +0900

    committtt

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from PIL import Image
 
 import frqi
 import quantum_edge_detection as qed
-import swap
+
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q-keio', group='keio-internal', project='keio-students')
 
 anc = QuantumRegister(1, "anc")
 img = QuantumRegister(11, "img")
@@ -20,11 +22,11 @@ c = ClassicalRegister(12)
 qc = QuantumCircuit(anc, img, anc2, c)
 
 imageNames = ["Ref_Tokyo_grayscale.jpg", "Tokyo_grayscale.jpg", "Sapporo_grayscale.jpg"]
-imgnum1 = 0
-imgnum2 = 2
+imageNum1 = 0
+imageNum2 = 2
 
-img1 = Image.open(imageNames[imgnum1]).convert('LA')
-img2 = Image.open(imageNames[imgnum2]).convert('LA')
+image1 = Image.open(imageNames[imageNum1]).convert('LA')
+image2 = Image.open(imageNames[imageNum2]).convert('LA')
 
 def image_normalization(image):
     image = resizeimage.resize_cover(image, [32, 32])
@@ -41,5 +43,50 @@ def image_normalization(image):
 
     return generated_image
 
-img1 = image_normalization(img1)
-img2 = image_normalization(img2)
+image1 = image_normalization(image1)
+image2 = image_normalization(image2)
+
+
+# apply hadamard gates
+for i in range(1, len(img)):
+    qc.h(img[i])
+
+# encode ref image
+for i in range(len(image1)):
+        if image1[i] != 0:
+                frqi.c10ry(qc, 2 * image1[i], format(i, '010b'), img[0], anc[0], [img[j] for j in range(1,len(img))])
+
+qc = qed.quantum_edge_detection(qc)
+
+qc.measure(range(12),range(12))
+
+backend_sim = Aer.get_backend('qasm_simulator')
+#print(qc.depth())
+numOfShots = 1024000
+result = execute(qc, provider.get_backend('ibmq_qasm_simulator'), shots=numOfShots).result()
+#circuit_drawer(qc).show()
+#plot_histogram(result.get_counts(qc))
+
+print(result.get_counts(qc))
+
+# generated image
+genimg = np.array([])
+
+#### decode
+for i in range(len(image1)):
+        try:
+                genimg = np.append(genimg,[np.sqrt(result.get_counts(qc)[format(i, '010b')+'10']/numOfShots)])
+        except KeyError:
+                genimg = np.append(genimg,[0.0])
+
+# inverse nomalization
+genimg *= 32.0 * 255.0
+
+# convert type
+genimg = genimg.astype('int')
+
+# back to 2-dimentional data
+genimg = genimg.reshape((32,32))
+
+plt.imshow(genimg, cmap='gray', vmin=0, vmax=255)
+plt.show()
--- swap.py
+++ swap.py
@@ -10,13 +10,6 @@ from PIL import Image
 
 import frqi
 
-imageNames = ["Ref_Tokyo_grayscale.jpg", "Tokyo_grayscale.jpg", "Sapporo_grayscale.jpg"]
-imgnum1 = 0
-imgnum2 = 2
-
-img1 = Image.open(imageNames[imgnum1]).convert('LA')
-img2 = Image.open(imageNames[imgnum2]).convert('LA')
-
 def image_normalization(image):
     image = resizeimage.resize_cover(image, [32, 32])
     w, h = 32, 32
@@ -31,22 +24,6 @@ def image_normalization(image):
     generated_image = np.arcsin(image)
 
     return generated_image
-
-img1 = image_normalization(img1)
-img2 = image_normalization(img2)
-
-IBMQ.load_account()
-provider = IBMQ.get_provider(hub='ibm-q-keio', group='keio-internal', project='keio-students')
-
-targetQubit = QuantumRegister(1, 'target')
-ref = QuantumRegister(11, 'ref')
-original = QuantumRegister(11, 'original')
-anc = QuantumRegister(1, 'anc')
-
-c = ClassicalRegister(1)
-
-qc = QuantumCircuit(targetQubit, ref, original, anc, c)
-
 
 def swap_12(circuit, q0, q1, q2, classic):
     
@@ -66,24 +43,48 @@ def swap_12(circuit, q0, q1, q2, classic):
 
     return result.get_counts(circuit)
 
-# apply hadamard gates
-for i in range(1, len(ref)):
-    qc.h(ref[i])
+if __name__ == '__main__':
+    imageNames = ["Ref_Tokyo_grayscale.jpg", "Tokyo_grayscale.jpg", "Sapporo_grayscale.jpg"]
+    imgnum1 = 0
+    imgnum2 = 2
 
-for i in range(1, len(original)):
-    qc.h(original[i])
+    img1 = Image.open(imageNames[imgnum1]).convert('LA')
+    img2 = Image.open(imageNames[imgnum2]).convert('LA')
+    
+    img1 = image_normalization(img1)
+    img2 = image_normalization(img2)
 
-#qc.h([ref[i] for i in range(1,len(ref))])
-#qc.h([original[i] for i in range(1,len(original))])
+    IBMQ.load_account()
+    provider = IBMQ.get_provider(hub='ibm-q-keio', group='keio-internal', project='keio-students')
 
-# encode ref image
-for i in range(len(img1)):
-        if img1[i] != 0:
-                frqi.c10ry(qc, 2 * img1[i], format(i, '010b'), ref[0], anc[0], [ref[j] for j in range(1,len(ref))])
+    targetQubit = QuantumRegister(1, 'target')
+    ref = QuantumRegister(11, 'ref')
+    original = QuantumRegister(11, 'original')
+    anc = QuantumRegister(1, 'anc')
 
-# encode original image
-for i in range(len(img2)):
-        if img2[i] != 0:
-                frqi.c10ry(qc, 2 * img2[i], format(i, '010b'), original[0], anc[0], [original[j] for j in range(1,len(original))])
+    c = ClassicalRegister(1)
 
-print(swap_12(qc, targetQubit, ref, original, c))
+    qc = QuantumCircuit(targetQubit, ref, original, anc, c)
+
+
+    # apply hadamard gates
+    for i in range(1, len(ref)):
+        qc.h(ref[i])
+
+    for i in range(1, len(original)):
+        qc.h(original[i])
+
+    #qc.h([ref[i] for i in range(1,len(ref))])
+    #qc.h([original[i] for i in range(1,len(original))])
+
+    # encode ref image
+    for i in range(len(img1)):
+            if img1[i] != 0:
+                    frqi.c10ry(qc, 2 * img1[i], format(i, '010b'), ref[0], anc[0], [ref[j] for j in range(1,len(ref))])
+
+    # encode original image
+    for i in range(len(img2)):
+            if img2[i] != 0:
+                    frqi.c10ry(qc, 2 * img2[i], format(i, '010b'), original[0], anc[0], [original[j] for j in range(1,len(original))])
+
+    print(swap_12(qc, targetQubit, ref, original, c))
